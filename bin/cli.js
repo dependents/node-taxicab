5b629115480148ba475d7995c9f356e80f8b4ffc
Joel Kemp <jokemp@adobe.com>
2014-07-24 18:11:35 -0400

    Pre support for generating a table of profiling tree generation for all driver scripts

--- bin/cli.js
+++ bin/cli.js
@@ -2,18 +2,23 @@
 
 'use strict';
 
-var findDriver = require('../'),
-    filename = process.argv[2],
-    root     = process.argv[3],
-    config   = process.argv[4];
+var findDriver  = require('../'),
+    filename    = process.argv[2],
+    root        = process.argv[3],
+    config      = process.argv[4];
 
 try {
-  findDriver(filename, root, function(drivers) {
-    drivers.forEach(function(driver) {
-      console.log(driver);
-    });
-  },
-  config);
+  findDriver({
+    filename: filename,
+    root: root,
+    config: config,
+    success: function(drivers) {
+      drivers = drivers || [];
+      drivers.forEach(function(driver) {
+        console.log(driver);
+      });
+    }
+  });
 } catch (e) {
   console.log(e);
 }
--- index.js
+++ index.js
@@ -1,83 +1,164 @@
-var getDriverScripts = require('amd-driver-scripts'),
+var amdDriverScripts  = require('amd-driver-scripts'),
     getModulesToBuild = require('get-modules-to-build'),
-    getTreeAsList = require('dependency-tree').getTreeAsList,
-    path = require('path'),
-    q = require('q');
+    getTreeAsList     = require('dependency-tree').getTreeAsList,
+    path              = require('path'),
+    q                 = require('q'),
+    Table             = require('cli-table'),
+    timer             = require('node-tictoc');
 
 /**
  * Gets the driver script(s) that depend on the given module
- * @param  {String} file      - The path of the module whose driver script should be found
- * @param  {String} root      - The path where all JS files are
- * @param  {Function} cb - (String[]) Executed with the driver scripts that depend on the given file
- * @param  {String} [config]  - Path to the requirejs config (to avoid recomputing all driver scripts)
+ * @param {Object}   options
+ * @param {String}   options.filename  - The path of the module whose driver script should be found
+ * @param {String}   options.root      - The path where all JS files are
+ * @param {Function} options.success   - (String[]) Executed with the driver scripts that depend on the given file
+ * @param {String}   [options.config]  - Path to the requirejs config (to avoid recomputing all driver scripts)
  */
-module.exports = function(file, root, cb, config) {
-  var drivers;
+module.exports = function(options) {
+  if (!options.filename)  throw new Error('file not given');
+  if (!options.root)      throw new Error('root location not given');
+  if (!options.success)   throw new Error('callback not given');
 
-  if (!file)  throw new Error('file not given');
-  if (!root)  throw new Error('root location not given');
-  if (!cb)    throw new Error('callback not given');
+  options.filename = path.resolve(process.cwd(), options.filename);
 
-  file = path.resolve(process.cwd(), file);
+  getDriverScripts(options)
+  .then(function(drivers) {
+    options.drivers = drivers;
+    return options;
+  })
+  .then(findRelatedDrivers)
+  .done(options.success);
+};
 
-  if (config) {
-    drivers = getModulesToBuild(config);
-    drivers = resolvePaths(drivers, root);
+/**
+ * Determines how long it takes to generate the dependency trees for each of the
+ * @param  {[type]} options [description]
+ * @return {[type]}         [description]
+ */
+function timeToGenerateTrees(options) {
+  var table = new Table({
+      head: ['Module', 'Tree time (ms)'],
+      colWidths: [50, 20]
+  });
 
-    drivers = drivers.map(function(driver) {
-      return driver.indexOf('.js') === -1 ?
-             driver + '.js' :
-             driver;
-    });
+  options.profile = true;
+  options.table = table;
 
-    findRelatedDrivers(drivers, file, root).then(cb);
+  getTrees(options)
+  .then(function() {
+    console.log(table.toString());
+  });
+}
+
+/**
+ *
+ * @param  {Options} options
+ * @param  {Options} options.root
+ * @param  {Options} options.config
+ * @return {Promise} (String[]) => null - Resolves with a list of driver scripts
+ */
+function getDriverScripts(options) {
+  var deferred = q.defer();
+
+  if (options.config) {
+    deferred.resolve(getConfigDrivers(options.root, options.config));
 
   } else {
-    getDriverScripts(root, function(drivers) {
-      // console.log('DEDUCED DRIVERS: ', drivers)
-      findRelatedDrivers(drivers, file, root).then(cb);
+    amdDriverScripts(options.root, function(drivers) {
+      deferred.resolve(drivers);
     });
   }
-};
+
+  return deferred.promise;
+}
+
+/**
+ * Get the driver scripts from a requirejs configuration file
+ * @param  {String} root
+ * @param  {String} configPath
+ * @return {String[]}
+ */
+function getConfigDrivers(root, configPath) {
+  var drivers = getModulesToBuild(configPath);
+
+  drivers = resolvePaths(drivers, root);
+
+  drivers = drivers.map(function(driver) {
+    return driver.indexOf('.js') === -1 ?
+           driver + '.js' :
+           driver;
+  });
+
+  return drivers;
+}
 
 /**
  * Finds the driver scripts from the list that depend on the given
  * file (at some point in the dependency tree)
- * @param  {String[]} drivers
- * @param  {String} filename
- * @param  {String} root
- * @return {Promise} Resolves with a list of the relevant driver scripts
+ *
+ * @param  {Object}   options
+ * @param  {String[]} options.drivers
+ * @param  {String}   options.filename
+ * @param  {String}   options.root
+ * @return {Promise}  Resolves with a list of the relevant driver scripts
  */
-function findRelatedDrivers(drivers, filename, root) {
-  return getTrees(drivers, root)
-    .then(function(results) {
-      var relatedDrivers = [];
+function findRelatedDrivers(options) {
+  return q().then(function() {
+    return options;
+  })
+  .then(getTrees)
+  .then(function(trees) {
+    options.trees = trees;
+    return options;
+  })
+  .then(function() {
+    var relatedDrivers = [];
 
-      results.forEach(function(treeList, idx) {
-        if (treeList.indexOf(filename) !== -1) {
-          relatedDrivers.push(drivers[idx]);
-        }
-      });
-
-      return relatedDrivers;
+    options.trees.forEach(function(treeList, idx) {
+      if (treeList.indexOf(options.filename) !== -1) {
+        relatedDrivers.push(options.drivers[idx]);
+      }
     });
+
+    return relatedDrivers;
+  });
 }
 
 /**
  * Gets the dependency tree for each of the given files
- * @param  {String[]} drivers
- * @return {Promise} Resolves with a list of trees for each file in the list
+ * @param  {Object}     options
+ * @param  {String[]}   options.drivers
+ * @param  {String}     options.root
+ * @param  {Boolean}    options.profile - Whether or not to print profiling information
+ * @param  {cli-table}  options.table   - Table instance for pretty output
+ * @return {Promise}    (String[]) => null Resolves with a list of trees for each file in the list
  */
-function getTrees(drivers, root) {
-  return q.all(drivers.map(function(driver) {
-    var deferred = q.defer();
+function getTrees(options) {
+  return q.all(options.drivers.map(function(driver) {
+    var deferred = q.defer(),
+        time;
 
-    getTreeAsList(driver, root, deferred.resolve.bind(deferred));
+    if (options.profile) {
+      timer.tic();
+    }
+
+    getTreeAsList(driver, options.root, deferred.resolve.bind(deferred));
+
+    if (options.profile) {
+      time = timer.toct();
+      options.table.push([driver.split(options.root)[1], time.ms]);
+    }
 
     return deferred.promise;
   }));
 }
 
+/**
+ * Resolve each driver script path against the root
+ * @param  {String[]} drivers
+ * @param  {String} root
+ * @return {String[]}
+ */
 function resolvePaths(drivers, root) {
   return drivers.map(function(name) {
     return path.resolve(root, name);
