a2b1bfc8749f705e69a7429c6595b944abc9527c
Joel Kemp <mrjoelkemp@gmail.com>
2016-05-29 15:33:53 -0400

    Upgrade dependency tree
    * Webpack support
    * Requirejs config tree resolution
    * Documentation tweaks

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ var getDriverScripts = require('get-driver-scripts');
 var getTreeAsList = require('dependency-tree').toList;
 var path = require('path');
 var q = require('q');
+var debug = require('debug')('taxi');
 
 /**
  * Gets the driver script(s) that depend on the given module
@@ -9,12 +10,19 @@ var q = require('q');
  * @param {Object}   options
  * @param {String}   options.filename  - The path of the module whose driver script should be found
  * @param {String}   options.directory - The path to all JS files
+ * @param {String}   options.config - The path to a requirejs config
+ * @param {String}   options.webpackConfig - The path to a webpack config
  * @param {Function} options.success   - (String[]) Executed with the driver scripts that depend on the given file
  */
 module.exports = function(options) {
   if (!options.filename) { throw new Error('filename not given'); }
   if (!options.directory) { throw new Error('directory location not given'); }
   if (!options.success) { throw new Error('success callback not given'); }
+
+  debug('given filename: ' + options.filename);
+  debug('given directory: ' + options.directory);
+  debug('given config: ' + options.config);
+  debug('given webpackConfig: ' + options.webpackConfig);
 
   options.filename = path.resolve(options.filename);
 
@@ -24,8 +32,16 @@ module.exports = function(options) {
     var relatedDrivers = findRelatedDrivers({
       drivers: drivers,
       filename: options.filename,
-      directory: options.directory
+      directory: options.directory,
+      config: options.config,
+      webpackConfig: options.webpackConfig
     });
+
+    if (err) {
+      debug('error: ' + err.message);
+    }
+
+    debug('related drivers: \n' + relatedDrivers.join('\n'));
 
     success(err, relatedDrivers);
   };
@@ -44,9 +60,13 @@ module.exports = function(options) {
  * @return {Promise}  Resolves with a list of the relevant driver scripts
  */
 function findRelatedDrivers(options) {
-  var trees = getTrees({
-    drivers: options.drivers,
-    directory: options.directory
+  var trees = options.drivers.map(function(driver) {
+    return getTreeAsList({
+      filename: driver,
+      root: options.directory,
+      config: options.config,
+      webpackConfig: options.webpackConfig
+    });
   });
 
   var relatedDrivers = [];
@@ -58,19 +78,4 @@ function findRelatedDrivers(options) {
   });
 
   return relatedDrivers;
-}
-
-/**
- * Gets the dependency tree for each of the given files
- * @param  {Object}     options
- * @param  {String[]}   options.drivers
- * @param  {String}     options.directory
- * @return {String[]}
- */
-function getTrees(options) {
-  var cache = {};
-
-  return options.drivers.map(function(driver) {
-    return getTreeAsList(driver, options.directory, cache);
-  });
 }
